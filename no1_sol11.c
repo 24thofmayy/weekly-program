4da6a56c046b771133c254c328501cd1802edf1c
Patthanan <patthananchualam@gmail.com>
2022-08-16 02:49:31 +0700

    first commit

---
+++ no1_sol11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    printf("SUM : %d\n",num1 + num2);
+    printf("DIFFERRENCE : %d\n",num1 - num2);
+    printf("PRODUCT : %d\n",num1 * num2);
+    printf("QUOTIENT : %d",num1 / num2);
+    return 0;
+}
--- test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("hello bubu");
-    return 0;
-}
