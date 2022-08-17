8803b98f062c1bab14c6e92acf8bbed20e61f3d0
Patthanan <patthananchualam@gmail.com>
2022-08-17 21:58:33 +0700

    second commit

--- no7_sol1.c
+++ no7_sol1.c
@@ -5,21 +5,21 @@ int main() {
     printf("Enter 3 numbers : ");
     scanf("%d %d %d",&a,&b,&c);
 
-    if (a,c > b) 
+    if (a >= b && a >= c) 
     {
-        if (a > c)
-        {
-            printf("the greatest number is %d",a);
-        }
-        else
-        {
-            printf("the greatest number is %d",c);
-        }
-        
+        printf("the greatest number is %d",a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("the greatest number is %d",b);
+    }
+    else if (c >= a && c >= b)
+    {
+        printf("the greastest number is %d",c);
     }
     else
     {
-        printf("the greastest number is %d",b);
+        printf("error");
     }
     
     return 0;
