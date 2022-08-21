fb3f1cd647b6a1a4c3fe6e6dfee137c79711360f
Patthanan <patthananchualam@gmail.com>
2022-08-22 02:53:10 +0700

    first commit

---
+++ no20_sol2.c
@@ -0,0 +1,16 @@
+//จงเขียนโปรแกรมรับตัวเลข 1 ตัวแล้วให้คอมพิวเตอร์วาดรูปเครื่องหมาย * เป็นจำนวนเท่ากับตัวเลขที่รับเข้ามา
+#include <stdio.h>
+#include <stdio.h>
+int main() {
+    int n,i = 0;
+    printf("Enter number :");
+    scanf("%d",&n);
+
+    while (i < n)
+    {
+        printf("*");
+        i += 1;
+    }
+    
+    return 0;
+}
