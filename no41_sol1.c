1a87ee4db86021f661bf8fb02ddeb309ffeb416f
Patthanan <patthananchualam@gmail.com>
2022-09-16 04:03:01 +0700

    first commit

---
+++ no41_sol1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int max(int num1, int num2);
+
+int main() 
+{
+    int num1, num2, maximum, minimum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    max(num1, num2);
+    return 0;
+}
+
+int max(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        printf("Maximum = %d\n",num1 );
+    }
+    else if (num2 > num1)
+    {
+        printf("Maximum = %d\n",num2);
+    }
+}
