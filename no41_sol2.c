3b29b98593b3227274b1d8d6eae8781632f3813b
Patthanan <patthananchualam@gmail.com>
2022-09-16 04:29:14 +0700

    first commit

---
+++ no41_sol2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int largest(int num[2])
+{
+    int i;
+    int max = num[0];
+    for (i = 1; i < 2; i++)
+        if (num[i] > max)
+            max = num[i];
+ 
+    return max;
+}
+ 
+int main()
+{
+    int num[2] ;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&num[0],&num[1]);
+    printf("Maximum = %d", largest(num));
+    return 0;
+}
